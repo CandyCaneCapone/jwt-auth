dfc06df814e2bb27b560eba808eb632b6cc6ff43
amir hossein <amirfarvahar709@gmail.com>
2023-12-10 20:13:50 +0330

    update-auth-middleware

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,20 +1,22 @@
 const jwt = require("jsonwebtoken");
 const UnAuthenticated = require("../errors/unauthenticated");
 
-const isAuthenticated = async (req, res, next) => {
+const isAuthenticated = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  try {
-    if (!authHeader || !authHeader.startsWith("Bearer")) {
-      throw new UnAuthenticated("wrong authentication token");
-    }
 
-    const token = authHeader.split(" ")[1];
-    const decoded = await jwt.verify(token, process.env.SECRET_KEY);
-    req.user = { _id: decoded._id };
-    next();
-  } catch (error) {
-    next(error);
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new UnAuthenticated("Please provide your token");
   }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = { _id: decoded._id };
+  }catch(error) {
+    next(new UnAuthenticated("Please provide a valid token"))
+  }
+  next();
+  
 };
 
 module.exports = isAuthenticated;
